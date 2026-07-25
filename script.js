a42e9a26520aec76eb76666df84e7ac5af0b8ed2
falcongold83-dotcom <falcongold83@gmail.com>
2026-07-25 21:47:06 +0330

    Update script.js

--- script.js
+++ script.js
@@ -193,8 +193,8 @@ document.addEventListener("mousemove", (e) => {
 });
 
 function animateCursor() {
-    cursorX += (mouseX - cursorX) * 0.40;
-    cursorY += (mouseY - cursorY) * 0.40;
+cursorX = mouseX;
+cursorY = mouseY;
 
     cursor.style.left = cursorX + "px";
     cursor.style.top = cursorY + "px";
